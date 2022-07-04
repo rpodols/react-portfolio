66d568540c335e5b80c637ab64e323d77ef307a0
ryanp <rdpodols@gmail.com>
2022-07-04 14:24:02 -0400

    added contact page html to component js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,7 +3,22 @@ import React from 'react';
 function Contact() {
   return (
     <section>
-      <h1 id="contact">Contact</h1>
+      <h1 id="contact">Contact Me:</h1>
+        <form id="contact-form">
+            <div>
+                <label htmlFor="name">Name: </label>
+                <input type="text" name="name" />
+            </div>
+            <div>
+                <label htmlFor="email">Email Address: </label>
+                <input type="email" name="email" />
+            </div>
+            <div>
+                <label htmlFor="message">Message: </label>
+                <textarea name="message" rows="5" />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
     </section>
   );
 }
