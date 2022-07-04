699d0fbe22b3a66700c848dba2623fe50c454247
ryanp <rdpodols@gmail.com>
2022-07-04 14:43:27 -0400

    added contact component and form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About';
 import Navigation from './components/Nav';
 import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <About></About>
         <Portfolio></Portfolio>
+        <Contact></Contact>
       </main>
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
   return (
     <section>
       <h1 id="contact">Contact Me:</h1>
-        <form id="contact-form">
+        <form id="contact-form" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Name: </label>
-                <input type="text" name="name" />
+                <input type="text" defaultValue={name} onChange={handleChange} name="name" />
             </div>
             <div>
                 <label htmlFor="email">Email Address: </label>
-                <input type="email" name="email" />
+                <input type="email" defaultValue={email} name="email" onChange={handleChange} />
             </div>
             <div>
                 <label htmlFor="message">Message: </label>
-                <textarea name="message" rows="5" />
+                <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
             </div>
             <button type="submit">Submit</button>
         </form>
