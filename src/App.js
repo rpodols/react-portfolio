6057e5eac17d3624896fa015f768c74e242a9f9d
ryanp <rdpodols@gmail.com>
2022-07-04 11:05:12 -0400

    added portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './components/About';
 import Navigation from './components/Nav';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navigation></Navigation>
       <main>
         <About></About>
+        <Portfolio></Portfolio>
       </main>
     </div>
   );
