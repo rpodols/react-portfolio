6bd5ec8961cc607eae48214d34f6b41d88f73933
ryanp <rdpodols@gmail.com>
2022-07-08 17:04:34 -0400

    resume download link works to start pdf download

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,7 +5,7 @@ function Resume() {
     return (
     <section>
       <h1 id="resume">Resume</h1>
-      <a href="https://docs.google.com/document/d/0B44YFZvutHkjS3JJTGQ2ZGtXSmM/edit?usp=sharing&ouid=117673361416213673080&resourcekey=0-nH8k3v2J5wDVYttXgJWmpQ&rtpof=true&sd=true">Click here to download my resume!</a>
+      <a href="http://docs.google.com/document/d/0B44YFZvutHkjS3JJTGQ2ZGtXSmM/export?format=pdf">Click here to download my resume!</a>
     </section>
   );
 }
