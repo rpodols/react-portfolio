5ca764efd9e74ff4c661cbd5b93e0be7594b4925
ryanp <rdpodols@gmail.com>
2022-07-10 15:47:31 -0400

    updated resume link

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,7 +5,7 @@ function Resume() {
     return (
     <section>
       <h1 className="header-format" id="resume">Resume</h1>
-      <a className="header-format" href="http://docs.google.com/document/d/0B44YFZvutHkjS3JJTGQ2ZGtXSmM/export?format=pdf" target="_blank">Click here to download my resume!</a>
+      <a className="header-format" href="https://docs.google.com/document/d/0B44YFZvutHkjS3JJTGQ2ZGtXSmM/edit?usp=sharing&ouid=117673361416213673080&resourcekey=0-nH8k3v2J5wDVYttXgJWmpQ&rtpof=true&sd=true" target="_blank">Click here to view and download my resume!</a>
       <h3 className="header-format">Proficiencies:</h3>
       <p className="header-format">Include fundamentals of computer science applied to JavaScript, browser based technologies, databases, server side development, and API interactions.</p>
       <ul className="form-format">
