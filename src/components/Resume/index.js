c1ef3b9b07d6b9e1fd5d52c63c21cafaaa4c67ae
ryanp <rdpodols@gmail.com>
2022-07-10 15:27:57 -0400

    refactored resume download to open new tab

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,7 +5,7 @@ function Resume() {
     return (
     <section>
       <h1 className="header-format" id="resume">Resume</h1>
-      <a className="header-format" href="http://docs.google.com/document/d/0B44YFZvutHkjS3JJTGQ2ZGtXSmM/export?format=pdf">Click here to download my resume!</a>
+      <a className="header-format" href="http://docs.google.com/document/d/0B44YFZvutHkjS3JJTGQ2ZGtXSmM/export?format=pdf" target="_blank">Click here to download my resume!</a>
       <h3 className="header-format">Proficiencies:</h3>
       <p className="header-format">Include fundamentals of computer science applied to JavaScript, browser based technologies, databases, server side development, and API interactions.</p>
       <ul className="form-format">
